a1f087c7f22b8c953c24eaad6e6dd0edd7a4b1b3
iSenity1812 <pvduchuya4@gmail.com>
2024-10-07 23:02:58 +0700

    add dark-mode

--- index.js
+++ index.js
@@ -6,4 +6,25 @@ document.addEventListener("DOMContentLoaded", function() {
     menuToogle.addEventListener("click", function() {
         navLinks.classList.toggle("active");
     });
+    
+    // Light and Dark
+    const themeToggle = document.querySelector("#mode-toggle");
+    const mainContent = document.querySelector("#main-content");
+
+    // Function to toggle between light and dark mode
+    function toggleTheme() {
+        mainContent.classList.toggle("dark-mode");
+        const isDark = mainContent.classList.contains("dark-mode");
+        localStorage.setItem("theme", isDark ? "dark" : "light");
+    }
+
+    // Function to apply the saved theme
+    const saveTheme = localStorage.getItem("theme");
+    if (saveTheme === "dark") {
+        mainContent.classList.add("dark-mode");
+        themeToggle.checked = true;
+    }
+
+    // Add event listener to the theme toggle
+    themeToggle.addEventListener("change", toggleTheme);
 });
